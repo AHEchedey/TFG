ce6852fa55922a8bed67113cf77b36f1c979adee
AHEchedey <ahechedey@gmail.com>
2025-04-20 21:58:45 +0200

    cambios realizados de eliminar antiguo workspace

--- TFG_ws/build/room3/CMakeFiles/room3_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/room3_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/room3_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- TFG_ws/build/turtlebot4_simulator/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/echedey/Desktop/TFG/TFG_ws/install/turtlebot4_description/share/turtlebot4_description/package.sh"
-. "/home/echedey/Desktop/TFG/TFG_ws/install/turtlebot4_gz_toolbox/share/turtlebot4_gz_toolbox/package.sh"
-. "/home/echedey/Desktop/TFG/TFG_ws/install/turtlebot4_gz_bringup/share/turtlebot4_gz_bringup/package.sh"
